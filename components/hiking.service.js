3109e5bc1bec0fe93bbfa526795456529ef45da0
admin <alex.darmos@gmail.com>
2019-06-10 21:03:02 -0400

    cleanup

--- components/hiking.service.js
+++ components/hiking.service.js
@@ -2,7 +2,6 @@ function HikingService($http, $q) {
     const service = this;
     console.log("hello");
     service.favoriteArray = [];
-
     service.key = '200488347-7449e5616f0f75c446c24d3c0da3ba39';
     service.geoKey = 'AIzaSyAzWLrTiTrHUeTKCGNNpPkFLVrJ-ncycK0';
 
@@ -13,13 +12,11 @@ function HikingService($http, $q) {
             lon: localtionLon,
             key: service.key
         }
-
         return $q(function (resolve, reject) {
             $http({
                 url: url,
                 method: 'GET',
                 params: apiParam,
-
             })
                 .then((response) => {
                     console.log("it worked!!!!!!")
@@ -34,8 +31,6 @@ function HikingService($http, $q) {
         })
     }
 
-    //https://maps.googleapis.com/maps/api/geocode/json?address=grand+rapids&key=AIzaSyAzWLrTiTrHUeTKCGNNpPkFLVrJ-ncycK0
-//data.results[""0""].geometry.location
     service.getGeocode = (search) => {
         let url = 'https://maps.googleapis.com/maps/api/geocode/json';
         let apiParam = {
@@ -48,14 +43,12 @@ function HikingService($http, $q) {
                 url: url,
                 method: 'GET',
                 params: apiParam,
-
             })
                 .then((response) => {
                     service.location = {
                         lat:  response.data.results[0].geometry.location.lat,
                         lon: response.data.results[0].geometry.location.lng
                     }
-
                     console.log("geocode worked!!!!!!")
                     console.log(response.data.results[0].geometry.location)
                     resolve(service.location );
@@ -68,9 +61,6 @@ function HikingService($http, $q) {
         })
     }
 
-
-
-
     service.setFavorites = (favoriteParam) =>{
         service.favoriteArray.push(favoriteParam);
     }
@@ -79,7 +69,6 @@ function HikingService($http, $q) {
     }
     service.getGeocode().then( (response) => {
         service.getTrails(response.lat, response.lon);
-
     });
 }
 
