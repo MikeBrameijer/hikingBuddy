7fc0664046b4f782f45fef2e3e0b6909e600ad0d
brameijm <brameijm@gmail.com>
2019-06-13 21:53:47 -0400

    ngrepeat stuff

--- components/hiking-list/hikingList.js
+++ components/hiking-list/hikingList.js
@@ -1,9 +1,6 @@
 function HikingListController(hikingService) {
     const ctrl = this;
 
-    // ctrl.search = (location) => {
-    //     hikingService.getGeocode(location) 
-    // }
     ctrl.getList = (location) => {
         console.log(location);
         hikingService.getGeocode(location) 
@@ -11,6 +8,7 @@ function HikingListController(hikingService) {
 
                 console.log("it worked in hikingList.js");
                 console.log(results);
+                ctrl.trailsArray = results;
 
             })
             .catch((err) => {
@@ -18,16 +16,8 @@ function HikingListController(hikingService) {
                 console.log(err);
             });
     }
-
-   
-
 }
- // <div class="container" id="searchResults">
-            // test
-            //     <div class="container" ng-repeat="trail in $ctrl.hikingList">
-            //         <h2>{{trail.name}}</h2>
-            //     </div>
-            // </div>
+ 
 
 angular
     .module('HikingApp')
@@ -35,6 +25,32 @@ angular
         template: `
             <p> this is the hiking list component </p>
             <search-component search-rec="$ctrl.getList(que)"></search-component>
+
+            <div class="container" id="searchResults">
+                <div class="container" ng-repeat="trail in $ctrl.trailsArray">
+                    <h2>{{trail.name}}</h2>
+                    <a ng-if="trail.imgSmallMed != ''" href="{{trail.url}}"><img src="{{trail.imgSmallMed}}"/></a>
+                    <p>Rating: {{trail.stars}} stars </p>
+                    <p>Difficulty: {{trail.difficulty}} </p>
+                    <p>length: {{trail.length}} miles</p>
+                    <div class="container">
+                    Elevation
+                    <br>
+                    Ascent: {{trail.ascent}} ft
+                    <br>
+                    Descent: {{trail.descent}} ft
+                    <br>
+                    High: {{trail.high}} ft
+                    <br>
+                    Low: {{trail.low}} ft
+                    <br>
+                    </div> 
+                    <p ng-if="trail.summary != 'Needs Summary' && trail.summary != 'Needs Adoption'">
+                        Summary: {{trail.summary}} </p>
+                    
+                    <br>
+                </div>
+            </div>
            
     `, 
         controller: HikingListController,
