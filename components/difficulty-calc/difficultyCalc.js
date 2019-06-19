8e7c3c4df69664e8e66f29daf8dd76ab1ad9a628
unknown <megan.gallimore@gmail.com>
2019-06-19 18:40:31 -0400

    changes

--- components/difficulty-calc/difficultyCalc.js
+++ components/difficulty-calc/difficultyCalc.js
@@ -15,23 +15,18 @@ function DifficultyCalcController() {
         ctrl.waterIntake();
         ctrl.calculateCalories(170);
 
-        ctrl.calculateDifficulty("Novice");
-        ctrl.trailDifficultyConv(); 
-
-        ctrl.responseToTrail(ctrl.trail.stars);
-
-
     }
  
     ctrl.calculateTime = () => {        
-        ctrl.totalHikeTime = Math.round(parseInt((ctrl.trail.length * 30) + ((ctrl.trail.ascent/1000) * 30)));
+        ctrl.totalHikeTime = ((ctrl.trail.length * 30) + ((ctrl.trail.ascent/1000) * 30));
         if (ctrl.totalHikeTime >= 60) {
-            ctrl.totalHikeTimeFormat = (ctrl.totalHikeTime/60) +' hours';
+            ctrl.totalHikeTimeFormat = parseInt(ctrl.totalHikeTime/60) +' hours';
         } else {
-            ctrl.totalHikeTimeFormat = (ctrl.totalHikeTime) + ' minutes';
+            ctrl.totalHikeTimeFormat = parseInt(ctrl.totalHikeTime) + ' minutes';
+            ctrl.formatTime = Math.floor(parseInt(ctrl.totalHikeTimeFormat));
         }
 
-        return ctrl.totalHikeTimeFormat;
+        return Math.floor(parseInt(ctrl.totalHikeTimeFormat));
     }
 
     ctrl.waterIntake = () => {
@@ -53,108 +48,38 @@ function DifficultyCalcController() {
         return ctrl.totalCalsBurnFormat;
     }
 
-    ctrl.trailDifficultyConv = () => {
-        if(ctrl.trail.difficulty === "green"){
-            ctrl.difficultyConv = "No obstacles. Flat."
-        }
-        if(ctrl.trail.difficulty === "greenBlue"){
-            ctrl.difficultyConv = "Some sections of uneven terrain. Mostly flat."
-        }
-        if(ctrl.trail.difficulty === "blue"){
-            ctrl.difficultyConv = "Uneven terrain. Small inclines (max 10% grade)."
-        }
-        if(ctrl.trail.difficulty === "blueBlack"){
-            ctrl.difficultyConv = "Some obstacles such as rocks or roots present. Moderate inclines."
-        }
-        if(ctrl.trail.difficulty === "black"){
-            ctrl.difficultyConv = "Tricky terrain. Steep. Not for beginners (max 15% grade)."
-        }
-        if(ctrl.trail.difficulty === "dblack"){
-            ctrl.difficultyConv = "Potentially hazardous terrain. Very steep. Experts only."
-        }
-        return ctrl.difficultyConv;
-    }
-
-    ctrl.calculateDifficulty = (expLvl) => {
-        // formula from https://www.hikingincolorado.org/hikecalc.html
-        // Rating (Novice) = ( 0.002 x elevation gain [ in feet ] ) + round trip distance [ in miles ]
-        // Rating (Expert) = ( 0.0005 x elevation gain [ in feet ] ) + round trip distance / 2 [ in miles ]
-        ctrl.hikerExpLvl = expLvl;
+    ctrl.calculateDifficulty = () => {
         
-        if(ctrl.hikerExpLvl === "Novice"){
-            ctrl.difficultyRating = (.002 * ctrl.trail.ascent) + ctrl.trail.length;
-        }
-        if(ctrl.hikerExpLvl === "Expert"){
-            ctrl.difficultyRating = (.0005 * ctrl.trail.ascent) + (ctrl.trail.length / 2);
-        }
-        console.log("difficultyRating " + ctrl.difficultyRating);
-        
-        if(ctrl.difficultyRating <= 5){
-            ctrl.difficultySuggestion = "an easy";
-        }else if(ctrl.difficultyRating <= 9){
-            ctrl.difficultySuggestion = "a moderate";
-        }else if(ctrl.difficultyRating <= 9){
-            ctrl.difficultySuggestion = "a Strenuous or Difficult";
-        }
-        console.log("difficultySuggestion " + ctrl.difficultySuggestion);
-        return ctrl.difficultySuggestion;
     }
-
-
-    ctrl.responseToTrail = (stars) => {
-        ctrl.roundedStars = Math.round(stars);
-        ctrl.trailResponse = "Default Text";
-        if (ctrl.roundedStars == 5) {
-            ctrl.trailResponse = "This trail is really popular!"
-        } else if (ctrl.roundedStars == 4) {
-            ctrl.trailResponse = "Most hikers enjoy this trail quite a bit."
-        } else if (ctrl.roundedStars == 3) {
-            ctrl.trailResponse = "This is a pretty average trail, you might enjoy it."
-        } else if (ctrl.roundedStars == 2) {
-            ctrl.trailResponse == "Be careful, a lot of hikers didn't enjoy this hike."
-        } else if (ctrl.roundedStars == 1) {
-            ctrl.trailResponse = "Beware, This trail might have a lot of issues."
-        } else {
-            ctrl.trailResponse = "This trail needs to be reviewed! Maybe you can help? :)"
-        }
-        return ctrl.trailResponse;
-    }
-
 }
     // 8oz every 30 minutes
     // prehydtrate 2 hours before 20 oz
-
-
-angular.module("HikingApp")
-.component("difficultyCalc", {
-    template: `
-    <div ng-if="$ctrl.showAssistant" class="window"></div>
-    <div ng-if="$ctrl.showAssistant" class="show">
-        <h2>Hiking Buddy</h2>
-        <h3>{{$ctrl.trail.name}}</h3>
-        {{ $ctrl.trailResponse }}
-        <br>
-        This {{$ctrl.trail.length}} mile trail should take you about {{$ctrl.totalHikeTimeFormat}}.
-        <br>
-        I would recommend that you take {{$ctrl.totalWaterIntakeFormat}} of water.
-        <br>
-        You will probaly burn about {{$ctrl.totalCalsBurnFormat}} so bring some trail mix.
-        <br>
-
-        This trail has a {{$ctrl.trail.difficulty}} meaning {{$ctrl.difficultyConv}}.
-        <br>
-        Personally I think that if you are a Novice hiker this will be {{$ctrl.difficultySuggestion}} hike.
-
-
-
-    
-    </div>
-    <button ng-click="$ctrl.show()">Show Assistant</button>
-    <button ng-click="$ctrl.hide()">Hide Assistant</button>
-    
-    `,
-    controller: DifficultyCalcController,
-    bindings: {
-        trail: '<'
-    }
-})
+    angular.module("HikingApp")
+    .component("difficultyCalc", {
+        template: `
+        <div ng-if="$ctrl.showAssistant" class="window"></div>
+        <div ng-if="$ctrl.showAssistant" class="show">
+            <h2>Hiking Buddy</h2>
+            <h3>{{$ctrl.trail.name}}</h3>
+            {{ $ctrl.trailResponse }}
+            <br>
+            This {{$ctrl.trail.length}} mile trail should take you about {{$ctrl.totalHikeTimeFormat}}.
+            <br>
+            I would recommend that you take {{$ctrl.totalWaterIntakeFormat}} of water.
+            <br>
+            You will probaly burn about {{$ctrl.totalCalsBurnFormat}} so bring some trail mix.
+            <br>
+            This trail has a {{$ctrl.trail.difficulty}} meaning {{$ctrl.difficultyConv}}.
+            <br>
+            Personally I think that if you are a Novice hiker this will be {{$ctrl.difficultySuggestion}} hike.
+        
+        </div>
+        <button ng-click="$ctrl.show()">Show Assistant</button>
+        <button ng-click="$ctrl.hide()">Hide Assistant</button>
+        
+        `,
+        controller: DifficultyCalcController,
+        bindings: {
+            trail: '<'
+        }
+    })
