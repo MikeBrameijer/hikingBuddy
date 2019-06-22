3ab0275e0809b60e13a17b80e5daaa718b8d31cd
admin <alex.darmos@gmail.com>
2019-06-21 20:13:44 -0400

    updated filter and styled sorting

--- components/hiking-list/hikingList.js
+++ components/hiking-list/hikingList.js
@@ -13,6 +13,7 @@ function HikingListController(hikingService) {
                     let trailsObj = {
                         id: value.id,
                         caloriesBurned: null,
+                        hikingTime: null,
                         lat: value.latitude,
                         lon: value.longitude,
                         name: value.name,
@@ -74,17 +75,21 @@ angular
     .component('hikingList', {
         template: `
 
-            <search-component search-rec="$ctrl.getList(que, maxDistance, minLength, minStars)"></search-component>
+        <search-component search-rec="$ctrl.getList(que, maxDistance, minLength, minStars)">           
+        </search-component>
+
 
             <div class="mainContainer" id="searchResults">
 
-                <select class="sort-trail" ng-model="sorting">
-                    <option selected="selected" value="stars">Stars- Low to High</option>
-                    <option value="-stars">Stars- High to Low</option>
-                    <option value="-caloriesBurned">Calories- High to Low </option>
-                    <option value="caloriesBurned">Calories- Low to High </option>
+                <select ng-model="sorting">
+                <option value=" ">Filter By:</option>
+                <option value="-hikingTime">Hike Time- High to Low</option>
+                <option value="hikingTime">Hike Time- Low to High</option>
+                <option value="-caloriesBurned">Calories- High to Low </option>
+                <option value="caloriesBurned">Calories- Low to High </option>
                 </select>
 
+            
                 <div class="container" ng-repeat="trail in $ctrl.trailsArray | orderBy: sorting track by trail.id" ng-class="{true: 'fullView', false: 'partialView'}[trail.showDetails == true]">
 
                 {{trail.caloriesBurned}}
